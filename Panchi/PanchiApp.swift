a7ce3f979dd7c746d11dacdf8a6817373de276a9
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2023-05-24 12:35:56 -0400

    Initial Commit

---
+++ Panchi/PanchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanchiApp.swift
+//  Panchi
+//
+//  Created by Panchi on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct PanchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
