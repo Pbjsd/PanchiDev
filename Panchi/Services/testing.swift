d255baaa49c6de4883228bf5cebf541fa9b6035f
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2023-09-15 13:14:42 -0400

    doing a test commit for a testing file to see if everything is committing properly

---
+++ Panchi/Services/testing.swift
@@ -0,0 +1,18 @@
+//
+//  testing.swift
+//  Panchi
+//
+//  Created by Panchi on 9/15/23.
+//
+
+import SwiftUI
+
+struct testing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testing()
+}
